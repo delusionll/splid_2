b735d1361a8005c9875f431730e315c1ed989491
delusion <lev.zernov@icloud.com>
2022-10-15 00:13:12 +0300

    создание класса юзеров

---
+++ splid_2/code/SplidList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splid_2.code
+{
+    class SplidList
+    {
+        private int sum;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private int Sum
+        {
+            get { return sum; }
+            set { sum = value; }
+        }
+        
+    }
+}
---
+++ splid_2/code/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splid_2.code
+{
+    internal class Users
+    {
+        private string username;
+        private int userbalance;
+
+        
+    }
+}
