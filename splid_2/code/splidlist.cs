fe7e487490b54df305b50a29d60b5182446336d0
delusion <lev.zernov@icloud.com>
2022-10-15 00:05:26 +0300

    добавил колонку примечаний (описаний)

--- splid_2/code/splidlist.cs
+++ splid_2/code/splidlist.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace splid_2.code
 {
-    internal class splidlist
+    class SplidList
     {
+        private int sum;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private int Sum
+        {
+            get { return sum; }
+            set { sum = value; }
+        }
+        
     }
 }
