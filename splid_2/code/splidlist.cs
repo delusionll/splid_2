9591053c1db3d2b09e6938b266b33f8afc7a4967
delusion <lev.zernov@icloud.com>
2022-10-14 23:57:09 +0300

    создал класс с базовым классом списка трат

---
+++ splid_2/code/splidlist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splid_2.code
+{
+    internal class splidlist
+    {
+    }
+}
