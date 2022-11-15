c0764d3973b5fac0cfb888f0decad8f160fca8e6
delusion <lev.zernov@icloud.com>
2022-11-15 15:15:44 +0300

    1Связал модельку и таблицу

--- splid_2/MainWindow.xaml.cs
+++ splid_2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using splid_2.code;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,33 @@ namespace splid_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<SplidList> splidListData;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Splid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Splid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void splidList_Loaded(object sender, RoutedEventArgs e)
+        {
+            splidListData = new BindingList<SplidList>()
+            {
+                new SplidList(){Description = "test"},
+                new SplidList(){Description = "asdasd", Sum = 14},
+                new SplidList(){Sum = 123}
+            };
+
+            splidList.ItemsSource = splidListData;
         }
     }
 }
--- splid_2/code/splidlist.cs
+++ splid_2/code/splidlist.cs
@@ -10,12 +10,20 @@ namespace splid_2.code
     class SplidList
     {
         private int sum;
+        private string text;
+
         public DateTime CreationDate { get; set; } = DateTime.Now;
 
-        private int Sum
+        public int Sum
         {
             get { return sum; }
             set { sum = value; }
+        }
+
+        public string Description
+        {
+            get { return text; }
+            set { text = value; }
         }
         
     }
